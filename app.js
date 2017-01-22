b7074045c4581e9d0eeb58f4064acf19c3ddfeb7
MinhtheHome <minhthe.007@gmail.com>
2017-01-22 23:00:20 +0700

    week 1: intro the nodejs Driver

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-var http = require('http');
+var MongoClient = require('mongodb').MongoClient;
+    assert = require('assert');
 
 
-var server = http.createServer(function(req, res){
-    res.writeHead(200,{'contain-type':'text/plain'});
-    res.end("hello world");
+MongoClient.connect('mongodb://localhost:27017/video',function(err,db){
+    assert.equal(null,err);
+    console.log("Successful connected to Server");
+
+    // Find some documents in our collection
+    db.collection('movies').find({}).toArray(function(err, docs){
+        //Print the title of the documents in the result set
+        docs.forEach(function(doc){
+            console.log(doc.title);
+        });
+        db.close();
+    });
+
+    console.log("Called find()");
 });
-
-server.listen(8000);
