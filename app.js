b95a3fe690bef93db0f1e884b229994be1d994c7
minhthe <minhthe.007@gmail.com>
2017-01-23 18:44:36 +0700

    week 1 : all together now (put all the router in the request connection mongodb)

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 var express = require('express');
     app = express();
+    engines = require('consolidate');
+    MongoClient = require('mongodb').MongoClient;
+    assert = require('assert');
 
-app.get('/', function(req, res){
-    res.send("Hello World");
-});
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
-app.use(function(req, res){
+MongoClient.connect('mongodb://localhost:27017/video',function(err, db){
+    assert.equal(null,err);
+    console.log("Successful connected to MongoDB.");
+
+    app.get('/', function(req, res){
+        db.collection('movies').find({}).toArray(
+            function(err,docs){
+                res.render('movies',{'movies':docs});
+            }
+        );
+    });
+
+    app.use(function(req, res){
     res.sendStatus(404);
-});
+    });
 
 var server = app.listen(3000, function(){
     var port = server.address().port;
     console.log('Express server listening on port %s',port);
 })
+
+});
