0b9ca48134b70f0203bdc0fa66a0774e36143b7e
minhthe <minhthe.007@gmail.com>
2017-01-23 16:35:00 +0700

    week 1 : Hello World using express

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-var MongoClient = require('mongodb').MongoClient;
-    assert = require('assert');
+var express = require('express');
+    app = express();
 
-
-MongoClient.connect('mongodb://localhost:27017/video',function(err,db){
-    assert.equal(null,err);
-    console.log("Successful connected to Server");
-
-    // Find some documents in our collection
-    db.collection('movies').find({}).toArray(function(err, docs){
-        //Print the title of the documents in the result set
-        docs.forEach(function(doc){
-            console.log(doc.title);
-        });
-        db.close();
-    });
-
-    console.log("Called find()");
+app.get('/', function(req, res){
+    res.send("Hello World");
 });
+
+app.use(function(req, res){
+    res.sendStatus(404);
+});
+
+var server = app.listen(3000, function(){
+    var port = server.address().port;
+    console.log('Express server listening on port %s',port);
+})
